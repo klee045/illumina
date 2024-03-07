96b017b470de447d7c793f16d73e17ce531c7f1f
kaai.en <66959182+klee045@users.noreply.github.com>
2024-03-07 23:46:44 +0800

    Revert "Feature/operations"

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,12 @@
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import CardNumber from "./common/models/CardNumber";
-import Operation from "./common/models/Operation";
 import NumberGeneratorService from "./services/NumberGeneratorService/NumberGeneratorService";
-import OperationGeneratorService from "./services/OperationGeneratorService/OperationGeneratorService";
-
-dotenv.config();
 
 const app: Express = express();
+dotenv.config();
+
 const port: string | 4000 = process.env.PORT || 4000;
-const numberGeneratorService = new NumberGeneratorService();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
@@ -19,19 +16,10 @@ app.get("/", (req: Request, res: Response) => {
  * Card Numbers Routes
  */
 app.get("/card_numbers", (req: Request, res: Response<CardNumber>) => {
-  const numberOne = numberGeneratorService.generateNumber();
-  const numberTwo = numberGeneratorService.generateNumber();
+  const numberOne = NumberGeneratorService.generateNumber();
+  const numberTwo = NumberGeneratorService.generateNumber();
 
   return res.status(200).json({ numberOne: numberOne, numberTwo: numberTwo });
-});
-
-/**
- * Operation Routes
- */
-app.get("/operation", (req: Request, res: Response<Operation>) => {
-  const operation = OperationGeneratorService.generateOperation();
-
-  return res.status(200).json({ operation: operation });
 });
 
 app.listen(port, () => {
--- backend/src/services/OperationGeneratorService/OperationGeneratorService.ts
+++
@@ -1,9 +0,0 @@
-class OperationGeneratorService {
-  public static generateOperation(): string {
-    const operations: Array<string> = ["+", "-", "x", "÷"];
-
-    return operations[Math.floor(Math.random() * operations.length)];
-  }
-}
-
-export default OperationGeneratorService;
